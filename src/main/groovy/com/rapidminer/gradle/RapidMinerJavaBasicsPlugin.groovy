c9616cad3291a443183666e0506798a7ef5f0cb5
Nils Wöhler <nwoehler@rapidminer.com>
2014-07-25 17:20:09 +0200

    Adds src/generated/java to main source set, use JUnit version 4.+, adds updateTestTimestamps tasks

--- src/main/groovy/com/rapidminer/gradle/RapidMinerJavaBasicsPlugin.groovy
+++ src/main/groovy/com/rapidminer/gradle/RapidMinerJavaBasicsPlugin.groovy
@@ -1,5 +1,6 @@
 package com.rapidminer.gradle
 
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -12,7 +13,7 @@ import org.gradle.api.Project
 class RapidMinerJavaBasicsPlugin implements Plugin<Project> {
 
 	private static final String ENCODING = 'UTF-8'
-	private static final String JAVA_COMPATIBILITY = 1.7
+	private static final String JAVA_COMPATIBILITY = JavaVersion.VERSION_1_7
 	private static final String DEFAULT_BUILD_DIR = "target"
 
 	@Override
@@ -22,20 +23,29 @@ class RapidMinerJavaBasicsPlugin implements Plugin<Project> {
 			apply plugin: 'java'
 			apply plugin: 'eclipse'
 			apply plugin: 'maven-publish'
-			
+
 			// ###################
 			// Used to create Maven like provided configuration
 			// See http://issues.gradle.org/browse/GRADLE-784
-			configurations {
-			    provided
-			}
-			
+			configurations { provided }
+
 			sourceSets {
-			    main.compileClasspath += configurations.provided
-			    test.compileClasspath += configurations.provided
-			    test.runtimeClasspath += configurations.provided
+				main.compileClasspath += configurations.provided
+				test.compileClasspath += configurations.provided
+				test.runtimeClasspath += configurations.provided
 			}
-			
+
+			//TODO add external source sets
+			/*
+			 * Extend the main source set by adding generated java and generated resources
+			 */
+			sourceSets {
+				main {
+					java { srcDir 'src/generated/java' }
+					resources { srcDir 'src/generated/resources' }
+				}
+			}
+
 			eclipse.classpath.plusConfigurations += [configurations.provided]
 
 			// set compilation encoding
@@ -51,12 +61,24 @@ class RapidMinerJavaBasicsPlugin implements Plugin<Project> {
 			targetCompatibility = JAVA_COMPATIBILITY
 
 			// add unit test settings
-			dependencies { testCompile 'junit:junit:4.11' }
+			dependencies { testCompile 'junit:junit:4.+' }
 
 			test {
 				// set system properties, as they are null by default
 				systemProperties = System.properties
 			}
+
+			// ensure that each Jenkins build sees updated test results (fails otherwise)
+			tasks.create(name: 'updateTestTimestamps') {
+				inputs.files test.outputs.files
+				doLast{
+					def timestamp = System.currentTimeMillis()
+					if(test.hasProperty('testResultsDir')){
+						test.testResultsDir?.eachFile { it.lastModified = timestamp }
+					}
+				}
+			}
+			check.dependsOn(updateTestTimestamps)
 
 			// create and configure sourceJar task
 			tasks.create(name: 'sourceJar', type: org.gradle.api.tasks.bundling.Jar)
@@ -71,14 +93,14 @@ class RapidMinerJavaBasicsPlugin implements Plugin<Project> {
 				classifier = 'javadoc'
 				from javadoc.destinationDir
 			}
-			
+
 			// specify artifacts
 			artifacts {
 				jar
 				sourceJar
 				javadocJar
 			}
-			
+
 			publishing {
 				publications {
 					jar(org.gradle.api.publish.maven.MavenPublication) { from components.java }
